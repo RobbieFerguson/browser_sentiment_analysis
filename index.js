8dc653d38f3ba1c9408e5587aed7c95c3f4b895b
Tyler Nappy <tylernappy@Tylers-MacBook-Pro-2.local>
2015-05-19 14:23:11 -0400

    added standin API calls

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http);
-var MyClient = require('idol-client')(process.env.idolOnDemandApiKey);
+var iod = require('iod-node')
+client = new iod.IODClient('http://api.idolondemand.com', process.env.idolOnDemandApiKey)
 
 var port = process.env.PORT || 5000
+
+var hexValue;
 
 app.use(express.static(__dirname + "/"))
 
@@ -17,34 +20,20 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
    socket.on('talk', function(msg){
-      console.log(msg)
-   //
-      console.log("save file")
-   //
 
-   //
-      console.log("get sentiment analysis")
-      // MyClient.recognizeSpeech({
-      //  method: 'POST',
-      //  type: 'async',
-      //  parameters: {
-      //      file: 'myFileName',
-      //  },
-      //  files: {
-      //      myFileName: __dirname+'./audio_files' // See IDOL supported files.
-      //  }
-      // }).then(
-      //     function(res){
-      //         console.log(res.code);    // => 200-299
-      //         console.log(res.headers);    // => Response headers
-      //         console.log(res.data);    // => Response data
-      //     },
-      //     function(error){ console.log('Ups, some error occured:', error); }
-      // );
-      //
+      client.call('recognizespeech', {'file' : msg} ,function(err,resp,body) {
+         client.call('analyzesentiment', {'file' : body.content} ,function(err,resp,body) {
+            // socket.emit('message', hexValue()) //send data analysis
+            socket.emit('message', 1) //send data analysis
+         })
+      })
    });
 });
 
 http.listen(port, function(){
   console.log('listening on *:5000');
 });
+
+function hexValue() {
+
+}
